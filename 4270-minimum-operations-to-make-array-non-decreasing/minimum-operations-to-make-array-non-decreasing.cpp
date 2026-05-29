27a4908e707d4d551084c17c5e6abb62be508363
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-05-29 09:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 136.8 MB (62.64%) - LeetSync

---
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] > nums[i]) {
+                ans += (nums[i - 1] - nums[i]);
+            }
+        }
+        return ans;
+    }
+};
