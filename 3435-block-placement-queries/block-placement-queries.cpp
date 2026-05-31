938649471486fc63b96f51896de75170ebe49ad7
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-05-31 14:07:04 +0530

    Time: 790 ms (7.47%) | Memory: 384.3 MB (14.95%) - LeetSync

--- 3435-block-placement-queries/block-placement-queries.cpp
+++ 3435-block-placement-queries/block-placement-queries.cpp
@@ -1,98 +1,59 @@
 class Solution {
 public:
-
-    vector<int> seg;
-    const int MAXX = 50000;
-
-    void update(int node, int l, int r, int idx, int val) {
-        if (l == r) {
-            seg[node] = val;
+    vector<int>seg;
+    int n=50005;
+    void construct(){
+        seg.resize(4*n,0);
+    }
+    void updateSeg(int idx,int val,int i,int l,int r){
+        if(l==r){
+            seg[i]=val;
             return;
         }
-
-        int mid = (l + r) / 2;
-
-        if (idx <= mid)
-            update(2 * node, l, mid, idx, val);
-        else
-            update(2 * node + 1, mid + 1, r, idx, val);
-
-        seg[node] = max(seg[2 * node], seg[2 * node + 1]);
+        int mid=l+(r-l)/2;
+        if(idx<=mid){
+            updateSeg(idx,val,2*i+1,l,mid);
+        }
+        else{
+            updateSeg(idx,val,2*i+2,mid+1,r);
+        }
+        seg[i]=max(seg[2*i+1],seg[2*i+2]);
     }
 
-    int query(int node, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l)
-            return 0;
-
-        if (ql <= l && r <= qr)
-            return seg[node];
-
-        int mid = (l + r) / 2;
-
-        return max(
-            query(2 * node, l, mid, ql, qr),
-            query(2 * node + 1, mid + 1, r, ql, qr)
-        );
+    int querySeg(int start,int end,int i,int l,int r){
+        if(l>end || r<start) return 0;
+        if(l>=start && r<=end) return seg[i];
+        int mid=l+(r-l)/2;
+        return max(querySeg(start,end,2*i+1,l,mid),querySeg(start,end,2*i+2,mid+1,r));
     }
-
     vector<bool> getResults(vector<vector<int>>& queries) {
-
-        seg.resize(4 * (MAXX + 1), 0);
-
-        set<int> obstacles;
-        obstacles.insert(0);
-
-        // Build final obstacle configuration
-        for (auto &q : queries) {
-            if (q[0] == 1) obstacles.insert(q[1]);
-        }
-
-        vector<int> pos(obstacles.begin(), obstacles.end());
-        // at pos[i] we have a gap of (pos[i] - pos[i - 1]) which we are storing
-        // in the segment tree
-        for (int i = 1; i < (int)pos.size(); i++) {
-            update(1,0,MAXX,pos[i],pos[i] - pos[i - 1]);
-        }
-
-        vector<bool> ans;
-
-        for (int i = (int)queries.size() - 1; i >= 0; i--) {
-
-            if (queries[i][0] == 2) {
-
-                int x = queries[i][1];
-                int sz = queries[i][2];
-                // find a prev obstacle lesser than x, then we can fit the block
-                //before prev or between prev to x
-                auto it = prev(obstacles.upper_bound(x));
-
-                int prevObstacle = *it;
-                int best = query(1,0,MAXX,0,prevObstacle);
-
-                best = max(best, x - prevObstacle);
-                ans.push_back(best >= sz);
+        construct();
+        set<int>st;
+        st.insert(0);
+        vector<bool>result;
+        for(auto q : queries){
+            if(q[0]==1){
+                // add obstacle
+                int x=q[1];
+                auto it=st.upper_bound(x);
+                if (it != st.end()) {
+                    int next = *it;
+                    updateSeg(next,next-x,0,0,n-1);
+                 }       
+                int pre=*prev(it);
+                updateSeg(x,x-pre,0,0,n-1);
+                st.insert(x);
             }
-            else {
-
-                int x = queries[i][1]; // so we now remove x obstacle
-                auto it = obstacles.find(x);
-                int leftPos = *prev(it); //left of x
-
-                update(1,0,MAXX,x,0); // update gap at x to 0 or remove gap ending at x
-
-                auto rightIt = next(it);
-
-                if (rightIt != obstacles.end()) {
-                    int rightPos = *rightIt;
-                    // merging the interval from leftpos to rightpos as one single gap
-                    update(1,0,MAXX,rightPos,rightPos - leftPos);
-                }
-
-                obstacles.erase(it);
+            else{
+                int x=q[1];
+                int sz=q[2];
+                auto it =st.upper_bound(x);
+                int pre=*prev(it);
+                int maxGap=querySeg(0,pre,0,0,n-1);
+                int best=max(maxGap,x-pre);
+                result.push_back(best>=sz);
             }
         }
-
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return result;
     }
 };
