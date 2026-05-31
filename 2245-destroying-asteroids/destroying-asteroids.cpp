58147894531577b137b3c9554ece802ca5884122
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-05-31 20:17:17 +0530

    Time: 33 ms (53.87%) | Memory: 106.6 MB (99.12%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
         long long curr_mass=mass;
-        multiset<long long>rem(asteroids.begin(),asteroids.end());
-        while(!rem.empty()){
-            auto it=rem.upper_bound(curr_mass);
-            if(it==rem.begin()){
+        for(int num : asteroids){
+            if(num>curr_mass){
                 return false;
             }
-            it--;
-            curr_mass+=*it;
-            rem.erase(it);
+            else{
+                curr_mass+=num;
+            }
         }
         return true;
     }
