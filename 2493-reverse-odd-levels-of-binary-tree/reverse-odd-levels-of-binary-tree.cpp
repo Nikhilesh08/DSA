011f9a33a6d081f9be34c8d43cf7c6dccd0a97a8
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-06-05 20:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (91.16%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -1,51 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        int level=0;
-        while(!q.empty()){
-        int sz=q.size();
-        vector<TreeNode*>vec;
-        for(int i=0;i<sz;i++){
-            TreeNode*node=q.front();
-            q.pop();
-            if(level%2!=0){
-                vec.push_back(node);
-            }
-            if(node->left){
-                q.push(node->left);
-            }
-            if(node->right){
-                q.push(node->right);
-            }
+    void traverseAndSwap(TreeNode* leftNode, TreeNode* rightNode, int level) {
+        if (leftNode == nullptr || rightNode == nullptr) {
+            return;
         }
-        if(level%2!=0){
-            int low=0;
-            int high=vec.size()-1;
-            while(low<high){
-                TreeNode*first=vec[low];
-                TreeNode*end=vec[high];
-                int temp=first->val;
-                first->val=end->val;
-                end->val=temp;
-                low++;
-                high--;
-            }
+        if (level % 2 != 0) {
+            swap(leftNode->val, rightNode->val);
         }
-        level++;
+        traverseAndSwap(leftNode->left, rightNode->right, level + 1);
+        traverseAndSwap(leftNode->right, rightNode->left, level + 1);
     }
-    return root;
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) return nullptr;
+        traverseAndSwap(root->left, root->right, 1);
+        return root;
     }
 };
