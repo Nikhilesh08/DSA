0cd2b1b0c6a11934415b6b4f97be4ad1dc0be961
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-06-05 15:58:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Lexicographically smallest after removing k/lexicographically-smallest-after-removing-k.cpp
+++ Difficulty: Medium/Lexicographically smallest after removing k/lexicographically-smallest-after-removing-k.cpp
@@ -1,37 +1,36 @@
 class Solution {
-public:
-    bool isPower2(int n) {
-        return (n > 0) && ((n & (n - 1)) == 0);
-    }
-
+  public:
+  bool isPower2(int n){
+      return (n>0) && ((n & n-1)==0);
+  }
     string lexicographicallySmallest(string &s, int k) {
-        int n = s.size();
-        
-        if (isPower2(n)) {
-            k = k / 2;
-        } else {
-            k = k * 2;
-        }
-        
-        if (k >= n) return "-1";
-        
-        string ans = "";
-        
-        for (char ch : s) {
-            while (!ans.empty() && k > 0 && ans.back() > ch) {
-                ans.pop_back();
+        // code here
+        int n=s.size();
+        if(isPower2(n))k=k/2;
+        else k=k*2;
+        stack<char>st;
+        if(k>=n) return "-1";
+        for(int i=0;i<s.size();i++){
+            char curr=s[i];
+            while(!st.empty() && curr < st.top() && k>0){
+                st.pop();
                 k--;
             }
-            ans.push_back(ch);
+            st.push(s[i]);
         }
         
-        while (k > 0 && !ans.empty()) {
-            ans.pop_back();
+        while(k>0){
+            st.pop();
             k--;
         }
-        
-        if (ans.empty()) return "-1";
-        
+        string ans="";
+        while(!st.empty()){
+            char ch=st.top();
+            ans+=ch;
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.empty()) return "-1";
         return ans;
     }
 };
