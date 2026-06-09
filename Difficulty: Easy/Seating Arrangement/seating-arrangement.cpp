6049ff440ed309a704c7ee0560aebc4c6f398f44
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-06-09 09:40:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Seating Arrangement/seating-arrangement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool canSeatAllPeople(int k, vector<int> &seats) {
+        int n = seats.size();
+        int i = 0;
+        if (k == 0) return true;
+
+        while (i < n && k > 0) {
+            if (seats[i] == 0) {
+
+            bool leftEmpty = (i == 0 || seats[i - 1] == 0);
+            bool rightEmpty = (i == n - 1 || seats[i + 1] == 0);
+            if (leftEmpty && rightEmpty) {
+                seats[i] = 1; 
+                k--;          
+                i++; 
+                }
+            }
+            i++;
+        }
+        
+        return k == 0;
+    }
+};
