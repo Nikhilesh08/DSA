e65473544ce506a88f1f97a34914f4424afcbaec
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-06-13 12:19:13 +0530

    Time: 39 ms (81.11%) | Memory: 13 MB (81.11%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -14,7 +14,13 @@ public:
     }
     
     int numSquares(int n) {
-        dp.resize(n + 1, -1);
-        return solve(n);
+        dp.resize(n + 1, n);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i] = min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+        return dp[n];
     }
 };
