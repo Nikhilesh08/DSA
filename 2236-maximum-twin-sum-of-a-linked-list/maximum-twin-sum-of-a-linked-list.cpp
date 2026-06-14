4bc16ad62b39a59bf38e5c64c73af4e7868a1150
Nikhilesh08 <nikhileshkumargubba@gmail.com>
2026-06-14 13:57:13 +0530

    Time: 4 ms (56.70%) | Memory: 128.5 MB (38.57%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,43 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode*revList(ListNode*head){
-        ListNode*prev=NULL;
-        ListNode*next_ptr=head;
-        ListNode*curr=head;
-        while(next_ptr!=NULL){
-            next_ptr=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next_ptr;
+    ListNode* revList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next_ptr = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next_ptr;
         }
         return prev;
     }
     int pairSum(ListNode* head) {
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode*head2=revList(slow->next);
-        ListNode*curr1=head;
-        ListNode*curr2=head2;
-        int maxi=0;
-        while(curr1 && curr2){
-            maxi=max(maxi,curr1->val+curr2->val);
-            curr1=curr1->next;
-            curr2=curr2->next;
+        ListNode* head2 = revList(slow); 
+        ListNode* reversedHalfHead = head2; 
+
+        ListNode* curr1 = head;
+        ListNode* curr2 = head2;
+        int maxi = 0;
+        while (curr2 != nullptr) {
+            maxi = max(maxi, curr1->val + curr2->val);
+            curr1 = curr1->next;
+            curr2 = curr2->next;
         }
+        revList(reversedHalfHead);
+
         return maxi;
     }
 };
